ec430b3a6f7974953e127537718e1d8962182ca1
“Daliso <daliso.zuze@gmail.com>
2017-02-03 14:18:59 +0000

    Update

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,6 +1,8 @@
 import pickle
 import tensorflow as tf
 # TODO: import Keras layers you need here
+from keras.models import Sequential
+from keras.layers.core import Dense, Activation, Flatten
 
 flags = tf.app.flags
 FLAGS = flags.FLAGS
@@ -46,9 +48,26 @@ def main(_):
     # the dataset
     # 10 for cifar10
     # 43 for traffic
+    
+    from sklearn.preprocessing import LabelBinarizer
+    label_binarizer = LabelBinarizer()
+    y_one_hot = label_binarizer.fit_transform(y_train)
+    y_one_hot_val = label_binarizer.fit_transform(y_val)
+    
+    model = Sequential()
+    model.add(Flatten(input_shape=(1, 1, 2048)))
+    model.add(Dense(43))
+    model.add(Activation('softmax'))
 
     # TODO: train your model here
-
+    model.compile('adam', 'categorical_crossentropy', ['accuracy'])
+    history = model.fit(X_train, y_one_hot, nb_epoch=10)
+    
+    metrics = model.evaluate(X_val, y_one_hot_val)
+    for metric_i in range(len(model.metrics_names)):
+        metric_name = model.metrics_names[metric_i]
+        metric_value = metrics[metric_i]
+        print('{}: {}'.format(metric_name, metric_value))
 
 # parses flags and calls the `main` function above
 if __name__ == '__main__':
